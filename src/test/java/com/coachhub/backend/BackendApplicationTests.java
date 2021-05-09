08067f7b6c5643762199482a275ee70a836009c6
runembp <58623004+runembp@users.noreply.github.com>
2021-05-09 11:40:05 +0200

    Initial commit

---
+++ src/test/java/com/coachhub/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.coachhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
